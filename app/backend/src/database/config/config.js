568dfa2b0a163a855cc5a14c0aa361dac985d904
Rafael <RafaFriedelDev@outlook.com>
2022-12-29 10:51:14 -0300

    chore(back-end): :wrench: add sequelize configuration

---
+++ app/backend/src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'root',
+    database: 'cashforce_v3',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: 'root',
+    password: 'root',
+    database: 'cashforce_v3',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: 'root',
+    database: 'cashforce_v3',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
