d1aafed728b10968abb4bb4df23edd279deb65de
Rafael <RafaFriedelDev@outlook.com>
2022-12-29 11:22:35 -0300

    feat(front-end): :white_check_mark: implement browserrouter and provider in app

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MyProvider from './context/myProvider';
+import Orders from './pages/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MyProvider>
+        <Routes>
+          <Route path="/" element={<Orders />} />
+        </Routes>
+      </MyProvider>
+    </BrowserRouter>
   );
 }
 
