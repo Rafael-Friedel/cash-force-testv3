6d64fe4ba0494af6a2dbc7eb47e57227393af30d
Rafael <RafaFriedelDev@outlook.com>
2023-01-02 18:53:16 -0300

    feat(front-end): :white_check_mark: create ReceiptsTable components

---
+++ app/frontend/src/components/ReceiptsTable.jsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import myContext from '../context/myContext';
+import formatDate from '../helpers/formatDate';
+
+const ReceiptsTable = () => {
+  const { orders, listOfStatus } = useContext(myContext);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nota Fiscal</th>
+          <th>Sacado</th>
+          <th>Cedente</th>
+          <th>Emissão</th>
+          <th>Valor</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {orders.map((order) => (
+          <tr key={order.id}>
+            <td>{order.CNPJ.cnpj}</td>
+            <td>{order.buyer.name}</td>
+            <td>{order.provider.name}</td>
+            <td>{formatDate(order.emissionDate)}</td>
+            <td>{+order.value}</td>
+            <td>{listOfStatus[+order.orderStatusBuyer]}</td>
+            <button>Dados do cedente</button>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ReceiptsTable;
