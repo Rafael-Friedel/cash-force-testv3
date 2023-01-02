057484372296debf19369e072a6f98f14844bac5
Rafael <RafaFriedelDev@outlook.com>
2023-01-02 18:52:45 -0300

    feat(front-end): :white_check_mark: create Title components

---
+++ app/frontend/src/components/Title.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import HandIcon from '../assets/images/HandIcon';
+
+const Title = () => {
+  return (
+    <header>
+      <h1>
+        <HandIcon fill="#021B51" />
+        Notas Fiscais
+      </h1>
+      <h4>Visualize as notas fiscais que você tem:</h4>
+    </header>
+  );
+};
+
+export default Title;
