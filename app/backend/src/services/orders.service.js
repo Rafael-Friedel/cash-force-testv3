57f37386b7481c00440cecdf96a5ee71ce8c35ff
Rafael <RafaFriedelDev@outlook.com>
2023-01-02 18:01:47 -0300

    feat(back-end): :zap: integrates foreign tables

--- app/backend/src/services/orders.service.js
+++ app/backend/src/services/orders.service.js
@@ -1,8 +1,29 @@
-const { order } = require('../database/models');
+const { order, CNPJ, user, buyer, provider } = require('../database/models');
 
 class OrdersService {
   static async getAllOrders() {
-    const allOrders = await order.findAll();
+    const allOrders = await order.findAll({
+      include: [
+        {
+          model: user,
+          attributes: ['name'],
+        },
+        {
+          model: buyer,
+          attributes: ['name'],
+        },
+        {
+          model: CNPJ,
+          attributes: ['cnpj'],
+        },
+        {
+          model: provider,
+          attributes: ['name'],
+        },
+      ],
+      raw: true,
+      nest: true,
+    });
     return allOrders;
   }
 }
