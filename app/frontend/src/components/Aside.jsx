760a579ef263824b3b1027171dc816c19d4b1500
Rafael <RafaFriedelDev@outlook.com>
2023-01-03 18:50:13 -0300

    style(front-end): :lipstick: add style aside components

--- app/frontend/src/components/Aside.jsx
+++ app/frontend/src/components/Aside.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import HandIcon from '../assets/images/HandIcon';
 import logo from '../assets/images/logo-cashforce.svg';
+import style from '../css/aside.module.scss';
 
 const Aside = () => {
   return (
-    <aside>
-      <img src={logo} alt="Logo da CashForce" />
-      <nav>
+    <aside className={style.aside}>
+      <img src={logo} alt="Logo da CashForce" className={style.aside__logo} />
+      <nav className={style.aside__navbar}>
         <HandIcon fill="#00AD8C" />
-        <Link to="/orders">Notas Fiscais</Link>
+        <Link to="/orders" className={style.aside__navbar_link}>
+          Notas Fiscais
+        </Link>
       </nav>
     </aside>
   );
--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.module.scss';
+import './css/index.module.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
