63e4c6e0051ae32f9c05ad473b7acbd41f05362a
Rafael <RafaFriedelDev@outlook.com>
2023-01-03 12:26:56 -0300

    feat(back-end): :white_check_mark: creates cnpjsseeders

---
+++ app/backend/src/database/seeders/20230103145428-cnpj.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, _Sequelize) {
+    return queryInterface.bulkInsert(
+      'cnpjs',
+      [
+        {
+          id: 1,
+          cnpj: '00000000000001',
+          companyType: '2',
+          createdAt: '2020-10-29 21:20:33',
+          updatedAt: '2020-10-29 21:20:33',
+        },
+        {
+          id: 1,
+          cnpj: '00000000000002',
+          companyType: '1',
+          createdAt: '2020-10-29 21:20:33',
+          updatedAt: '2020-10-29 21:20:33',
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, _Sequelize) {
+    return queryInterface.bulkDelete('cnpjs', null, {});
+  },
+};
